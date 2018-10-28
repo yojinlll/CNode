363ae2e3be4046fffa7f8941ac1335d42bb8c5db
yojinlll <yojin1995@qq.com>
2018-10-28 17:19:17 +0800

    header 初步样式

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+  <c-header></c-header>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    'c-header':Header,
+  },
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <img src="../assets/cnodejs_light.svg">
+    <ul>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">新手入门</a></li>
+      <li><a href="#">API</a></li>
+      <li><a href="#">关于</a></li>
+      <li><a href="#">注册</a></li>
+      <li><a href="#">登录</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CNHeader',
+    data(){
+      return { msg:'fuck vue'}
+    }
+  }
+</script>
+
+<style scoped>
+  .header{
+    background-color: #444;
+    height: 50px;
+  }
+  img{
+    max-width: 120px;
+    margin-left: 67px;
+    margin-top: 8px;
+  }
+  ul{
+    list-style: none;
+    float: right;
+    margin: 4px;
+  }
+  li{
+    display: inline-block;
+    padding: 10px 15px;
+  }
+  a{
+    text-decoration: none;
+    color: #ccc;
+    font-size: 14px;
+  }
+
+</style>
